6643bf9981e324d61d5d2c74b4f14ba229c172dd
Deepak Agarwal <deepak.17jics065@jietjodhpur.ac.in>
2022-08-13 17:40:22 +0530

    Basics about JS

---
+++ main.js
@@ -0,0 +1,55 @@
+// alert('Button click');
+
+
+// var let and const    - - - variable 
+// var is globally scoped 
+
+// let and const with ES 2015 
+//let we can reassign the value 
+// const can be reassigned 
+
+// data type 
+// primitive data types
+
+// String , numbers , undefined , boolean , null
+// ; not mandatory in javascript
+let name ='John';
+let age = 30;
+let iscool = true;
+let isnull = null;
+let reading  = 4.65;   //will behave just as number 
+let z ; // undefined 
+
+let r =  undefined;
+
+// Template String
+
+console.log(`My name is = ${name} and my age is ${age}`)
+
+
+let string ='hellowrold';
+
+// if we wantto use a property then don't use () beacuse it will be a method 
+
+console.log("Lenght of string is " + string.length);
+
+console.log("string to uppercase : " + string.toUpperCase());
+
+// Arrays
+
+// arrays hold multiple value 2 ways to create
+
+// constructor method 
+
+let abc = new Array('23',3,34,6,5);
+
+console.log(abc);
+
+
+// other way to cretae 
+
+
+let fruits  = ['orages','apple'];
+
+console.log(fruits);
+
